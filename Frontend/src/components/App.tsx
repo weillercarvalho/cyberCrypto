9986ef0cf6e6a237df8b7a502a04915baff1e35f
Weiller Carvalho <engweiller@hotmail.com>
2023-03-01 02:52:32 -0300

    style: Starting signIn sass and react

--- Frontend/src/components/App.tsx
+++ Frontend/src/components/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      TESTE
-    </>
+  const [token, setToken] = useState("");
+  const auth = JSON.parse(localStorage.getItem(`crypto`) as any); // Operador de coalescencia nula nao surtiu efeito, nem ternario, nem verificacao com type of e nem operador de not null, sendo assim vai por bem ou por mal.
+  if (auth && token === "") {
+    return setToken(auth);
+  }
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signin' element={<SignIn />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/components/SignIn.tsx
@@ -0,0 +1,8 @@
+import "../styles/css/style.min.css"
+export default function SignIn() {
+    return (
+        <div className="signinback">
+
+        </div>
+    )
+}
