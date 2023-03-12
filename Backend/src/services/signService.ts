829be96d1b7f3d92444c05e167729c596a4d05e2
Weiller Carvalho <engweiller@hotmail.com>
2023-03-12 01:25:47 -0300

    feat: SignIn Controller/Middleware/Service and Repository done.

--- Backend/src/services/signService.ts
+++ Backend/src/services/signService.ts
@@ -19,14 +19,13 @@ export async function signUpService({
   return result;
 }
 
-export async function signInService({email, password}: typeSignIn) {
+export async function signInService({ email, password }: typeSignIn) {
   const getHashed = await hashVerification(email);
   if (!getHashed) {
-    throw new Error("Email not found!")
+    throw new Error("Email not found!");
   }
-  const comparePassword = bcrypt.compareSync(password, getHashed.password)
+  const comparePassword = bcrypt.compareSync(password, getHashed.password);
   if (!comparePassword) {
-    throw new Error("Wrong password!")
+    throw new Error("Wrong password!");
   }
-
 }
